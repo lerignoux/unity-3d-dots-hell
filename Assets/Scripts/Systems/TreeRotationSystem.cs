45786d81b1bd460f4aee8411f9367c41bc92bfed
Laurent Erignoux <lerignoux@gmail.com>
2022-12-10 09:42:50 +0800

    Adding rotating tree in subscene

--- Assets/Scripts/Systems/TreeRotationSystem.cs
+++ Assets/Scripts/Systems/TreeRotationSystem.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
-public class TreeRotation : MonoBehaviour
+// Unmanaged systems based on ISystem can be Burst compiled, but this is not yet the default.
+// So we have to explicitly opt into Burst compilation with the [BurstCompile] attribute.
+// It has to be added on BOTH the struct AND the OnCreate/OnDestroy/OnUpdate functions to be
+// effective.
+[BurstCompile]
+partial struct TurretRotationSystem : ISystem
 {
-    // Start is called before the first frame update
-    void Start()
+    // Every function defined by ISystem has to be implemented even if empty.
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    // Every function defined by ISystem has to be implemented even if empty.
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
     {
-        
+    }
+
+    // See note above regarding the [BurstCompile] attribute.
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        // The amount of rotation around Y required to do 360 degrees in 2 seconds.
+        var rotation = quaternion.RotateY(SystemAPI.Time.DeltaTime * math.PI);
+
+        // The classic C# foreach is what we often refer to as "Idiomatic foreach" (IFE).
+        // Aspects provide a higher level interface than directly accessing component data.
+        // Using IFE with aspects is a powerful and expressive way of writing main thread code.
+        foreach (var transform in SystemAPI.Query<TransformAspect>())
+        {
+            transform.RotateWorld(rotation);
+        }
     }
 }
